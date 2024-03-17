e372d2e6b4b947b6f9fa74a27e4be2aff6a3976c
Bình Phan <binhpc.21it@vku.udn.vn>
2024-03-17 17:08:02 +0700

    fix:language

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
-import express from "express"
-import bodyParser from "body-parser"
-import viewEngine from "./config/viewEngine"
-import router from "./route/web.js"
-import connectDB from "./config/connectDB"
-require("dotenv").config()
-import cors from "cors"
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine";
+import router from "./route/web.js";
+import connectDB from "./config/connectDB";
+require("dotenv").config();
+import cors from "cors";
 import cookieParser from "cookie-parser";
+import { WebhookClient } from "dialogflow-fulfillment";
 
 const app = express();
 
@@ -13,27 +14,18 @@ connectDB();
 
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 // app.use(cors({ credentials:true, origin:'http://localhost:3001' }));
 app.use(cors({ credentials: true, origin: true }));
 
 app.use(cookieParser());
 
-
-
-
 viewEngine(app);
 app.use(router);
 
-
-
-
-let port = process.env.PORT
+let port = process.env.PORT;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
-
-
+  console.log(`Server is running on port ${port}`);
+});
