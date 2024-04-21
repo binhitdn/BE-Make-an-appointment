e7ab546f01ed5e9a01746b99c80149cea46f5ae6
Bình Phan <binhpc.21it@vku.udn.vn>
2024-04-21 19:32:55 +0700

    delete:workspace

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,20 +1,23 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-
-
-const sequelize = new Sequelize('happycare', 'root', null, {
-  host: 'localhost',
-  dialect: 'mysql',
-  logging: false
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    logging: false,
+  }
+);
 
 let connectDB = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-      } catch (error) {
-        console.error('Unable to connect to the database:', error);
-      }
-}
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
 
 module.exports = connectDB;
--- src/migrations/99.migration-create-doctor-clinic-specialty.js
+++ src/migrations/99.migration-create-doctor-clinic-specialty.js
@@ -1,47 +1,38 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('doctors_clinics_specialties', {
-
+    await queryInterface.createTable("doctors_clinics_specialties", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       doctorId: {
         type: Sequelize.INTEGER,
         references: {
-          model: 'doctors',
-          key: 'id'
-        }
-      },
-      clinicId: {
-        type: Sequelize.INTEGER,
-        references: {
-          model: 'clinics',
-          key: 'id'
+          model: "doctors",
+          key: "id",
         },
-
       },
       specialtyId: {
         type: Sequelize.INTEGER,
         references: {
-          model: 'specialties',
-          key: 'id'
-        }
+          model: "specialties",
+          key: "id",
+        },
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('doctors_clinics_specialties');
-  }
+    await queryInterface.dropTable("doctors_clinics_specialties");
+  },
 };
